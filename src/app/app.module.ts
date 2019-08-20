f0fd9107d792c38bda23fdaf8f08314192e63547
Roman Manasyan <rmanasyan@gmail.com>
2019-08-20 17:21:45 +0300

    firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAEikInl3MJL-XfJB2cNS-QzsnGDA9dn0Q',
+    authDomain: 'xpenses-d6b34.firebaseapp.com',
+    databaseURL: 'https://xpenses-d6b34.firebaseio.com',
+    projectId: 'xpenses-d6b34',
+    storageBucket: 'xpenses-d6b34.appspot.com',
+    messagingSenderId: '269906377020',
+    appId: '1:269906377020:web:2566514be9771970'
+  }
 };
