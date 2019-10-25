1054da00072237aa9063f45df0223a1e0bae1005
Roman Manasyan <rmanasyan@gmail.com>
2019-10-25 18:49:35 +0300

    angular fire auth guard instead of custom guard for now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireAuthGuard } from '@angular/fire/auth-guard';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
 import { AkitaNgRouterStoreModule } from '@datorama/akita-ng-router-store';
@@ -21,7 +22,7 @@ import { environment } from '../environments/environment';
     environment.production ? [] : AkitaNgDevtools.forRoot(),
     AkitaNgRouterStoreModule.forRoot()
   ],
-  providers: [],
+  providers: [AngularFireAuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/transactions/transactions-routing.module.ts
+++ src/app/transactions/transactions-routing.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AngularFireAuthGuard, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
 
 import { AuthGuard } from '../auth/auth.guard';
 import { OverviewComponent } from './overview/overview.component';
 import { TransactionComponent } from './transaction/transaction.component';
 import { TransactionsComponent } from './transactions.component';
 
+const redirectUnauthorizedToAuth = () => redirectUnauthorizedTo(['auth']);
+
 const routes: Routes = [
   {
     path: '',
     component: TransactionsComponent,
-    canActivate: [AuthGuard],
+    // canActivate: [AuthGuard],
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToAuth },
     children: [
       { path: '', component: OverviewComponent },
       { path: 'transaction', component: TransactionComponent },
