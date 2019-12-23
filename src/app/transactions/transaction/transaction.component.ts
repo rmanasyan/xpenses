7af1f6669e8a710da64aa8bf132b5bde48da5625
Roman Manasyan <rmanasyan@gmail.com>
2019-12-23 20:48:42 +0200

    feat: back link on the transaction form

--- src/app/transactions/transaction/transaction.component.ts
+++ src/app/transactions/transaction/transaction.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Router } from '@angular/router';
 import { RouterQuery } from '@datorama/akita-ng-router-store';
 import { Observable } from 'rxjs';
+import { AuthQuery } from '../../auth/state/auth.query';
 import { Transaction } from '../state/transaction.model';
 import { TransactionsService } from '../state/transactions.service';
 import { TransactionsQuery } from '../state/transactions.query';
@@ -20,14 +21,11 @@ export class TransactionComponent implements OnInit {
     private router: Router,
     private routerQuery: RouterQuery,
     private transactionsQuery: TransactionsQuery,
-    private transactionsService: TransactionsService
+    private transactionsService: TransactionsService,
+    private authQuery: AuthQuery
   ) {}
 
   ngOnInit() {
-  }
-
-  discardTransaction() {
-    this.navigateBack();
   }
 
   removeTransaction(id: string) {
@@ -44,7 +42,11 @@ export class TransactionComponent implements OnInit {
     }
   }
 
+  get backLink() {
+    return this.authQuery.previousUrl;
+  }
+
   private navigateBack() {
-    this.router.navigate([`/categorized/${this.routerQuery.getParams('date')}`]);
+    this.router.navigate([this.backLink]);
   }
 }
